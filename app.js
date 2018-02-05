8c33819a1413cda3de3eccc82277d272c2552917
Stoodkev <quentin.correa@gmail.com>
2018-02-06 03:50:20 +0800

    force push
    Round
    
    Beneficiary
    
    Beneficiary
    
    Beneficiary
    
    Beneficiary

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 
 var app = express();
+require('dotenv').config();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
